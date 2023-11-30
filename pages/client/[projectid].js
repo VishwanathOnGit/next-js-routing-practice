8be7582b0c1ca9cbcf630d4fa2a571145819fe29
Vishwanath <putchavishwanath@gmail.com>
2023-11-30 21:36:10 +0530

    Added dynamic url and displayed on the page

--- pages/client/[projectid].js
+++ pages/client/[projectid].js
@@ -1,10 +1,16 @@
-function ClientProjectPage(params) {
+import { useRouter } from "next/router";
 
-    return (
-        <div>
-            <h1>Welcome to Client project for Next.js Course</h1>
-        </div>
-    );
+function ClientProjectPage(params) {
+  const router = useRouter();
+
+  let projectid = router.query.projectid;
+
+  return (
+    <div>
+      <h1>Welcome to Client project for Next.js Course</h1>
+      <p>Project ID: { projectid }</p>
+    </div>
+  );
 }
 
 export default ClientProjectPage;
